a6f59f3cd0f50b863d6cc00724aa680293703d5b
lee-kyubong <46523628+bong01@users.noreply.github.com>
2023-01-14 19:30:10 +0900

    [코드트리] k개 중에 1개를 n번 뽑기

---
+++ src/codetree/backtracking/k개_중에_1개를_n번_뽑기.java
@@ -0,0 +1,37 @@
+package codetree.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class k개_중에_1개를_n번_뽑기 {
+    static int k;
+    static int n;
+    static List<Integer> answer = new ArrayList<>();
+
+    public static void solution(int cnt) {
+        if (cnt == n) {
+            print();
+            return;
+        }
+        for (int i = 1; i <= k; i++) {
+            answer.add(i);
+            solution(cnt + 1);
+            answer.remove(answer.size() - 1);
+        }
+    }
+
+    public static void print() {
+        for (int num : answer) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        k = sc.nextInt();
+        n = sc.nextInt();
+        solution(0);
+    }
+}
