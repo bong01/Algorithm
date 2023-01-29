3d23b99f56ccfd1006914e1675796a13222ab653
lee-kyubong <46523628+bong01@users.noreply.github.com>
2023-01-29 13:41:47 +0900

    [코드트리] 거꾸로 순열

---
+++ src/codetree/backtracking/순열/거꾸로_순열.java
@@ -0,0 +1,35 @@
+package codetree.backtracking.순열;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class 거꾸로_순열 {
+    static int n;
+    static List<Integer> nums = new ArrayList<>();
+    static boolean[] visit;
+
+    public static void bt(int cnt) {
+        if (cnt == n) {
+            nums.forEach(x -> System.out.print(x + " "));
+            System.out.println();
+            return;
+        }
+
+        for (int i = n; i >= 1; i--) {
+            if (visit[i]) continue;
+            visit[i] = true;
+            nums.add(i);
+            bt(cnt + 1);
+            visit[i] = false;
+            nums.remove(nums.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        visit = new boolean[n + 1];
+        bt(0);
+    }
+}
