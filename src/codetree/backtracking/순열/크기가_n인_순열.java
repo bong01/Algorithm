e5890df38c96c1dad0d7271f4be5ca4de08218b3
lee-kyubong <46523628+bong01@users.noreply.github.com>
2023-01-29 12:06:21 +0900

    [코드트리] 크기가 n인 순열

---
+++ src/codetree/backtracking/순열/크기가_n인_순열.java
@@ -0,0 +1,42 @@
+package codetree.backtracking.순열;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class 크기가_n인_순열 {
+    static int n;
+    static List<Integer> list = new ArrayList<>();
+    static boolean[] visited;
+
+    public static void bt(int cnt) {
+        if (cnt == n) {
+            print();
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (visited[i]) continue;
+
+            visited[i] = true;
+            list.add(i);
+            bt(cnt + 1);
+            visited[i] = false;
+            list.remove(list.size() - 1);
+        }
+    }
+
+    public static void print() {
+        for (int e : list) {
+            System.out.print(e + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        visited = new boolean[n + 1];
+        bt(0);
+    }
+}
